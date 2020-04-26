32b60bbb6f1d2f947cb5601a11be8305592210d9
CoryF <coryf65@gmail.com>
2020-04-26 12:42:57 -0500

    map and list are good for working with data sets

--- BuiltIn_Functions/transforms.py
+++ BuiltIn_Functions/transforms.py
@@ -6,16 +6,27 @@ def filterFunc(x):
         return False
     return True
 
+# checks for if the given value is an uppercase
 def filterFunc2(x):
-    pass
+    if x.isupper():
+        return False
+    return True
 
-
+# returns X^2
 def squareFunc(x):
-    pass
+    return x**2
 
 
 def toGrade(x):
-    pass
+    if (x >= 90):
+        return 'A'
+    elif (x >= 80 and x < 90):
+        return 'B'
+    elif (x >= 70 and x < 80):
+        return 'C'
+    elif (x >= 65 and x < 70):
+        return 'D'
+    return 'F'
 
 
 def main():
@@ -25,15 +36,21 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
-
     odd_nums = list(filter(filterFunc, nums))
-    print(odd_nums)
+    print("odds: ", odd_nums)
 
     # TODO: use filter on non-numeric sequence
+    lower_chars = list(filter(filterFunc2, chars))
+    print("lower case values: ", lower_chars)
 
     # TODO: use map to create a new sequence of values
+    squares = list(map(squareFunc, nums))
+    print("Squares: ", squares)
 
     # TODO: use sorted and map to change numbers to grades
+    grades = sorted(grades)
+    letter_grade = list(map(toGrade, grades))
+    print("Letter Grades: ", letter_grade)
 
 
 if __name__ == "__main__":
