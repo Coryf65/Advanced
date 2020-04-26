bfe483bd705166724c0dc4034bd4f06c032f07cc
CoryF <coryf65@gmail.com>
2020-04-26 11:50:00 -0500

    Use iterators instead of the basic For Loops

--- BuiltIn_Functions/iterators.py
+++ BuiltIn_Functions/iterators.py
@@ -17,16 +17,33 @@ def main():
     # TODO: iterate using a function and a sentinel
 
     # Processing from testfile.txt
-    with open("testfile.txt", "r") as fp:
+    with open("BuiltIn_Functions/testfile.txt", "r") as fp:
         # it looks for the  empty string to indicate that is the end of our file
         for line in iter(fp.readline, ''):
             print(line)
 
     # TODO: use regular interation over the days
+    for m in days:
+        print(m)
+
+    # starting at number one more complex than ness
+    for m in range(len(days)):
+        print(m+1, days[m])
+
 
     # TODO: using enumerate reduces code and provides a counter
 
+    # using an enumerator can be more simple
+    for i,m in enumerate(days, start=1):
+        print(i, m)
+
     # TODO: use zip to combine sequences
+    for m in zip(days, daysFr):
+        print(m)
+
+    # TODO: use zip AND Enumerate
+    for i,m  in enumerate(zip(days, daysFr), start=1):
+        print(i, m[0], '=', m[1], 'in French')
 
 
 if __name__ == "__main__":
